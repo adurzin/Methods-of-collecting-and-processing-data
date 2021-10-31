0e1b088fd42734d2d4d26776fd8f0a0036a7efe4
Aleksey <a.durzin@yandex.ru>
2021-10-31 12:18:45 +0300

    lesson_3
    start
    
    
    lesson_3
    
    
    lesson_3

---
+++ lesson_3.py
@@ -0,0 +1,98 @@
+# https://voronezh.hh.ru/search/vacancy?text=Python&area=26&salary=&currency_code=RUR&experience=doesNotMatter&order_by=publication_time&search_period=0&items_on_page=20&no_magic=true&L_save_area=true&page=1
+
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+from pprint import pprint
+
+
+client = MongoClient('127.0.0.1', 27017)
+
+db = client['vacancy_list']
+vacancies = db.vacancies
+
+url = 'https://hh.ru/'
+
+vacancy = input('Введите название вакансии: ')
+
+params = {'text': vacancy,
+          'area': 26,
+          'clusters': 'true',
+          'enable_snippets': 'true',
+          'ored_clusters': 'true',
+          'experience': 'doesNotMatter',
+          'order_by': 'publication_time',
+          'search_period': 0,
+          'items_on_page': 20,
+          'no_magic': 'true',
+          'L_save_area': 'true',
+          'page': 0}
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                         'Chrome/81.0.4044.96 YaBrowser/20.4.0.1458 Yowser/2.5 Safari/537.36'}
+
+vacancy_list = []
+
+while True:
+    response = requests.get(url + 'search/vacancy', params=params, headers=headers)
+    dom = bs(response.content, 'html.parser')
+    vacancies = dom.find_all('div', {'class': "vacancy-serp-item"})
+
+    if response.ok and vacancies:
+        for vacancy in vacancies:
+            vacancy_data = {}
+            name = vacancy.find('a', {'data-qa': "vacancy-serp__vacancy-title"}).text
+            link = vacancy.find('a', {'data-qa': "vacancy-serp__vacancy-title"})['href']
+            try:
+                salary = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).text
+
+                currency = salary.split()[::-1][0]  # зп валюта
+                if salary.split()[0] == 'от':
+                    salary_min = int(''.join(salary.split()[1:-1]))  # если "от"
+                elif salary.split()[0] == 'до':
+                    salary_max = int(''.join(salary.split()[1:-1]))  # если "до"
+                else:
+                    salary_min = salary[:(salary.find(' – '))]
+                    salary_max = ''.join(salary[(salary.rfind(' – ') + 3):(salary.rfind(' '))])
+                    salary_min = int(''.join(salary_min.split()))  # минимальная зп
+                    salary_max = int(''.join(salary_max.split()))  # максимальная зп
+
+            except:
+                salary_min = None
+                salary_max = None
+                currency = None
+
+            vacancy_data['name'] = name  # наименование должности
+            vacancy_data['link'] = link  # ссылка на вакансию
+            vacancy_data['salary_min'] = salary_min  # минимальная зп
+            vacancy_data['salary_max'] = salary_max  # максимальная зп
+            vacancy_data['currency'] = currency  # валюта
+            vacancy_data['site'] = 'hh.ru'  # не совсем понял, что нужно в этом пункте
+            vacancy_data['_id'] = ''.join([en for en in link if en.isdigit()])  # добавляем id каждой вакансии
+            # vacancy_list.append(vacancy_data)
+
+            try:
+                db.vacancies.insert_one(vacancy_data)      # добавляем вакансию в базу данных
+            except DuplicateKeyError:
+                pass
+        print(f'Обработана страница № {params["page"]}')
+        params['page'] += 1
+    else:
+        break
+
+# df = pd.DataFrame(vacancy_list)
+# df.to_csv('data.csv')
+
+
+def salary(value, collection):
+    n = 0
+    for item in collection.find({'$or': [{'salary_min': {'$gt': value}}, {'salary_max': {'$gt': value}}]}):
+        pprint(item)
+        n += 1
+    print(f'Найдено {n} вакансий с зарплатой выше {value}')
+
+
+value_salary = int(input('Укажите необходимую зарплату: '))
+salary(value_salary, db.vacancies)
