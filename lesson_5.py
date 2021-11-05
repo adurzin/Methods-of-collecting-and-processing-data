7f2d332478b343f1c2b0ab9a6ac3346a8f1fe95f
adurzin <a.durzin@yandex.ru>
2021-11-05 19:15:42 +0300

    mvideo complete

--- lesson_5.py
+++ lesson_5.py
@@ -2,8 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-from time import sleep
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
 
+client = MongoClient('127.0.0.1', 27017)
+
+db = client['mvideo']
+products = db.products
 
 driver = webdriver.Chrome(executable_path='./chromedriver.exe')
 
@@ -17,16 +22,36 @@ button.click()
 
 while True:
     try:
-        element = driver.find_element(By.XPATH, '//mvid-shelf-group/*/div[2]/button[2]')
-        element.click()
-        sleep(1)
+        button = wait.until(ec.element_to_be_clickable((By.XPATH, '//mvid-shelf-group/*/div[2]/button[2]')))
+        button.click()
     except:
         break
 
-items = driver.find_elements(By.XPATH, "//mvid-shelf-group//div[@class='product-mini-card__name ng-star-inserted']")
+elements = driver.find_elements(By.XPATH, "//mvid-shelf-group/mvid-carousel/div[1]/div/mvid-product-cards-group/div")
 
+product = {}
 
-for element in items:
-    name = element.find_element(By.CLASS_NAME, "title").text
-    # price = element.find_element(By.XPATH, "///span[@class='price__main-value']").text
-    print(name)
+for el in elements:
+    try:
+        name = el.find_element(By.CLASS_NAME, 'title').text
+        link = el.find_element(By.TAG_NAME, 'a').get_attribute('href')
+        product['_id'] = link[:10:-1]
+        product['name'] = name
+        product['link'] = link
+    except:
+        pass
+
+    try:
+        price = el.find_element(By.CLASS_NAME, 'price__main-value').text
+        product['price'] = price
+
+        try:
+            db.products.insert_one(product)
+        except DuplicateKeyError:
+            print("Продукт уже существует в базе")
+        product = {}
+
+    except:
+        pass
+
+driver.quit()
