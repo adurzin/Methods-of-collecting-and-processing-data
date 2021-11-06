106b8f4a9fde33fc1d77a6ef8c9a9b78aed436eb
adurzin <a.durzin@yandex.ru>
2021-11-06 15:14:37 +0300

    mvideo complete

--- lesson_5.py
+++ lesson_5.py
@@ -4,19 +4,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from pymongo import MongoClient
 from pymongo.errors import DuplicateKeyError
+from selenium.common.exceptions import ElementNotInteractableException, TimeoutException
+
 
 client = MongoClient('127.0.0.1', 27017)
-
 db = client['mvideo']
 products = db.products
 
 driver = webdriver.Chrome(executable_path='./chromedriver.exe')
-
 driver.get('https://www.mvideo.ru/')
 
-driver.execute_script("window.scrollTo(0, 1800);")  # скролл на необходимую высоту
+driver.execute_script("window.scrollTo(0, 1900);")  # скролл на необходимую высоту
 
-wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 5)
 button = wait.until(ec.element_to_be_clickable((By.XPATH, "//div[1]/div/button[2]")))
 button.click()
 
@@ -24,34 +24,38 @@ while True:
     try:
         button = wait.until(ec.element_to_be_clickable((By.XPATH, '//mvid-shelf-group/*/div[2]/button[2]')))
         button.click()
-    except:
+    except (ElementNotInteractableException, TimeoutException):
         break
 
-elements = driver.find_elements(By.XPATH, "//mvid-shelf-group/mvid-carousel/div[1]/div/mvid-product-cards-group/div")
+elements = driver.find_elements(By.XPATH, "//mvid-shelf-group/mvid-carousel/div[1]//div[@class='title']")
 
 product = {}
 
+print(len(elements))
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                         " Chrome/95.0.4638.69 Safari/537.36"}
+
 for el in elements:
+    link = el.find_element(By.TAG_NAME, 'a').get_attribute('href')
+    print([link])
+    driver.execute_script(f'''window.open("{link}", "_blank");''')
+    driver.switch_to.window(driver.window_handles[1])
+    wait.until(ec.element_to_be_clickable((By.XPATH, "//mvideoru-cart-button/button | //mvid-mprime-button//button")))
+    name = driver.find_element(By.XPATH, "//h1[@class='title']").text
+    print(name)
+    price = driver.find_element(By.XPATH, "//span[@class='price__main-value'][1]").text
+    print(price)
+    product = {'link': link,
+               'name': name,
+               'price': price,
+               '_id': link[10::-1]
+               }
     try:
-        name = el.find_element(By.CLASS_NAME, 'title').text
-        link = el.find_element(By.TAG_NAME, 'a').get_attribute('href')
-        product['_id'] = link[:10:-1]
-        product['name'] = name
-        product['link'] = link
-    except:
-        pass
-
-    try:
-        price = el.find_element(By.CLASS_NAME, 'price__main-value').text
-        product['price'] = price
-
-        try:
-            db.products.insert_one(product)
-        except DuplicateKeyError:
-            print("Продукт уже существует в базе")
-        product = {}
-
-    except:
-        pass
+        db.products.insert_one(product)
+    except DuplicateKeyError:
+        print("Этот продукт уже есть в базе данных")
+    product = {}
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
 
 driver.quit()
