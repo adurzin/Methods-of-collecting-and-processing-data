c0565b0e83ab0d1d782c750e8802cf2adb19470f
Aleksei <a.durzin@yandex.ru>
2021-11-14 18:20:10 +0300

    start
    start
    
    start
    
    start
    
    start
    
    start
    
    start

---
+++ data_img/pipelines.py
@@ -0,0 +1,30 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import scrapy
+from itemadapter import ItemAdapter
+from scrapy.pipelines.images import ImagesPipeline
+
+
+class DataImgPipeline:
+    def process_item(self, item, spider):
+        print()
+        return item
+
+
+class LeroyPhotosPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        if item['photos']:
+            for img in item['photos']:
+                try:
+                    yield scrapy.Request(img)
+                except Exception as exc:
+                    print(exc)
+
+    def item_completed(self, results, item, info):
+        item['photos'] = [itm[1] for itm in results if itm[0]]
+        return item
