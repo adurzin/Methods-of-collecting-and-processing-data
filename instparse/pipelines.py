4d32cfa80860a3ee320850aba8ce38cbce660d82
Aleksey <a.durzin@yandex.ru>
2021-11-18 22:08:08 +0300

    13

--- instparse/pipelines.py
+++ instparse/pipelines.py
@@ -1,10 +1,3 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from pymongo import MongoClient
 
@@ -15,6 +8,6 @@ class InstparsePipeline:
         self.mongo_base = client['instagram']
 
     def process_item(self, item, spider):
-        collection = self.mongo_base[spider.name]
+        collection = self.mongo_base[item['user_type']]
         collection.update_one({'_id': item['_id']}, {'$set': item}, upsert=True)
         return item
