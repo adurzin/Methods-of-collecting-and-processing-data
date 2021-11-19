f7677e5052b400c80aab1ba5c4f275249e6e4ab2
Aleksei <a.durzin@yandex.ru>
2021-11-19 14:51:46 +0300

    complete

---
+++ instparse/query.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+client = MongoClient('localhost', 27017)
+db = client['instagram']
+
+
+user_for_query = 'griboochka'
+query_follow = 'follower'
+query = db[user_for_query].find({'user_type': query_follow})
+
+for el in query:
+    pprint(el)
